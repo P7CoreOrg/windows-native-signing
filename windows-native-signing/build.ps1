afc2541ddfaddd266613907398d7c2a0dbf39069
Herb Stahl <ghstahl@gmail.com>
2019-10-09 11:18:28 -0700

    don't use relative paths

--- windows-native-signing/build.ps1
+++ windows-native-signing/build.ps1
@@ -5,12 +5,11 @@ $clientSecret
 )
 Write-Host $clientId
 Write-Host $clientSecret
+Write-Host "HOME:"$PSScriptRoot
 
 dotnet tool install --global AzureSignTool --version 2.0.17
 
 
 
-Write-Information $clientId
-Write-Information $clientSecret
 
-AzureSignTool.exe sign  --file-digest sha256  --description-url "https://github.com/p7coreorg" --timestamp-rfc3161 http://timestamp.digicert.com --timestamp-digest sha256 --azure-key-vault-url https://P7KeyValut.vault.azure.net/ --azure-key-vault-client-id $clientId --azure-key-vault-client-secret $clientSecret  --azure-key-vault-certificate P7IdentityServer4SelfSigned .\Release\hello.utls.dll .\Release\HelloWorld.exe
+AzureSignTool.exe sign  --file-digest sha256  --description-url "https://github.com/p7coreorg" --timestamp-rfc3161 http://timestamp.digicert.com --timestamp-digest sha256 --azure-key-vault-url https://P7KeyValut.vault.azure.net/ --azure-key-vault-client-id $clientId --azure-key-vault-client-secret $clientSecret  --azure-key-vault-certificate P7IdentityServer4SelfSigned $PSScriptRoot\Release\hello.utls.dll $PSScriptRoot\Release\HelloWorld.exe
